8364cd99ce67ba0e958a846a21355c0f8afdb4ca
xyoouy <egorloparev@gmail.com>
2023-06-19 13:01:38 +0500

    Add TaskExecutor.cs

---
+++ Logic/Program.cs
@@ -0,0 +1,15 @@
+﻿using Logic;
+using TaskStatus = System.Threading.Tasks.TaskStatus;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        var taskExecutor = new TaskExecutor();
+
+        taskExecutor.ExecuteTaskAsync(null);
+        Task.Run(() => taskExecutor.ExecuteTaskAsync("First"));
+        Task.Run(() => taskExecutor.ExecuteTaskAsync("Second"));
+        Thread.Sleep(100000);
+    }
+}
---
+++ Logic/TaskExecutor.cs
@@ -0,0 +1,61 @@
+﻿namespace Logic;
+
+public class TaskExecutor
+{
+    private readonly object lockObject = new();
+    private Task<int> executionTask;
+        
+    private TaskCompletionSource<int> tcs;
+        
+    public async Task<int> ExecuteTaskAsync(object data)
+    {
+        lock (lockObject)
+        {
+            if (executionTask is {Status: TaskStatus.Running})
+            {
+                throw new InvalidOperationException("Task is already running");
+            }
+            Console.WriteLine(Thread.GetCurrentProcessorId());
+
+            executionTask = Task.Run(async () =>
+            {
+                await Task.Delay(5000);
+                if (data == null)
+                    throw new ArgumentNullException(nameof(data), "Data cannot be null");
+                    
+                return data.GetHashCode();
+            });
+               
+            tcs = new TaskCompletionSource<int>();
+                
+            executionTask.ContinueWith(task =>
+            {
+                if (task.Exception != null)
+                    tcs.SetException(task.Exception.InnerExceptions);
+                    
+            }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+            
+        try
+        {
+            var result = await executionTask;
+            tcs.SetResult(result);
+        }
+        catch (Exception ex)
+        {
+            tcs.SetException(ex);
+        }
+            
+        return await tcs.Task;
+    }
+
+    public TaskStatus GetTaskStatus() => executionTask.Status;
+
+    public int GetTaskResult()
+    {
+        if (executionTask.Status != TaskStatus.RanToCompletion)
+            throw new InvalidOperationException("Task is not yet completed");
+            
+        return executionTask.Result;
+    }
+}
